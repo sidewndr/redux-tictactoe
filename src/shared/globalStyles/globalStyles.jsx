e3a29d7f0f19e7db2e1027cbeac5ba4fc6e8c06d
sidewndr <anoshkinzz@mail.ru>
2021-09-09 17:32:07 +0300

    add font-size

--- src/shared/globalStyles/globalStyles.jsx
+++ src/shared/globalStyles/globalStyles.jsx
@@ -7,6 +7,7 @@ export const GlobalStyles = createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
     font-family: 'Open Sans', sans-serif;
+    font-size: 40px;
     background-color: black;
     color: white;
   }
