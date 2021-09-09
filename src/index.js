4848db4c3b772385b4b4ad753e685dc12858c6f1
sidewndr <anoshkinzz@mail.ru>
2021-09-09 17:34:21 +0300

    complete simple game

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import {GlobalStyles} from "./shared/globalStyles/globalStyles";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {GameField} from "./components/molecules/gameField";
 
 const App = () => {
   return (
     <Provider store={store}>
       <GlobalStyles />
+      <GameField />
     </Provider>
   )
 }
